78952cea2855d1656f2cebe1a5e91a5764b3188d
quarryman <quarrying@qq.com>
2023-05-06 00:39:14 +0800

    rename some files from utils_*.py to *_utils.py

--- khandy/__init__.py
+++ khandy/__init__.py
@@ -1,19 +1,17 @@
-from .utils_dict import *
-from .utils_feature import *
-from .utils_file_io import *
-from .utils_fs import *
-from .utils_hash import *
-from .utils_list import *
-from .utils_numpy import *
-from .utils_draw import *
-from .utils_others import *
-from .utils_split import *
-from .utils_time import *
+from .dict_utils import *
+from .draw_utils import *
+from .feature_utils import *
+from .file_io_utils import *
+from .fs_utils import *
+from .hash_utils import *
+from .list_utils import *
+from .misc import *
+from .numpy_utils import *
+from .split_utils import *
+from .time_utils import *
+from .version import *
 
 from .boxes import *
 from .image import *
 from .points import *
 from . import label
-
-from .version import *
-
